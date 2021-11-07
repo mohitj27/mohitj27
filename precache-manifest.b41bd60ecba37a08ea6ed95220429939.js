cd0f439ff6b356bd0ec40341e9ce0349efef89e8
Mohit Jain <mohitjain@Mohits-MacBook-Pro-2.local>
2021-11-07 19:05:44 +0000

    Updates

---
+++ precache-manifest.b41bd60ecba37a08ea6ed95220429939.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3000c0aafecc8a1ef2d56dd769d1b907",
+    "url": "/mohitjain.tech/index.html"
+  },
+  {
+    "revision": "8f87174e951436901d71",
+    "url": "/mohitjain.tech/static/css/2.b40bc0a3.chunk.css"
+  },
+  {
+    "revision": "8f87174e951436901d71",
+    "url": "/mohitjain.tech/static/js/2.cfb9c8fc.chunk.js"
+  },
+  {
+    "revision": "9edf093c9b2dd581ee4f86aacfe93f97",
+    "url": "/mohitjain.tech/static/js/2.cfb9c8fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e32f22216bc0a7599a",
+    "url": "/mohitjain.tech/static/js/3.e374b6c1.chunk.js"
+  },
+  {
+    "revision": "146ca9f33117181b3bc6",
+    "url": "/mohitjain.tech/static/js/4.8a84ecd4.chunk.js"
+  },
+  {
+    "revision": "821d7e1b76ef0eddb813",
+    "url": "/mohitjain.tech/static/js/5.101a07fb.chunk.js"
+  },
+  {
+    "revision": "22a807e8e96b2b918d90",
+    "url": "/mohitjain.tech/static/js/6.08068e28.chunk.js"
+  },
+  {
+    "revision": "8bf8b1b658b9c6d968f5",
+    "url": "/mohitjain.tech/static/js/main.8ec455de.chunk.js"
+  },
+  {
+    "revision": "1ba8be31805452d71890",
+    "url": "/mohitjain.tech/static/js/runtime-main.cddbed43.js"
+  },
+  {
+    "revision": "e99f24617f0ec1d0c0103c8df9d60d66",
+    "url": "/mohitjain.tech/static/media/certificates.e99f2461.md"
+  },
+  {
+    "revision": "2dae2509aea2b7471e5c353fab8997f2",
+    "url": "/mohitjain.tech/static/media/education.2dae2509.md"
+  },
+  {
+    "revision": "1acef612a957f3aad004fe0a8c5b654e",
+    "url": "/mohitjain.tech/static/media/projects.1acef612.md"
+  },
+  {
+    "revision": "d2738228758f7f8584e2833c39dff10b",
+    "url": "/mohitjain.tech/static/media/skills.d2738228.md"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mohitj27/precache-manifest.0b70672b11dfec600ca2418aaafd3e67.js"
+  "/mohitjain.tech/precache-manifest.b41bd60ecba37a08ea6ed95220429939.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mohitj27/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mohitjain.tech/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
